51e83dd33d3324474cd9d690f08e309b81462e52
Pavitra Katiyar <78842197+99003685@users.noreply.github.com>
2021-03-25 12:25:28 +0530

    Delete RestfulWebServices-1 directory

--- RestfulWebServices-1/src/main/java/com/ltts/Userrepository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ltts.Userrepository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.ltts.model.User;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-
-}
